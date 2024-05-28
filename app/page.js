ebad0e83be2f766380e1a3ab5752621a24e498d8
Lal Krishna N C <lalukrishnavallil01@gmail.com>
2024-05-28 07:16:27 +0530

    sidebar added

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import ResponsiveDrawer from "@/components/sidebar";
 
 export default function Home() {
-  return (
-   <>petroman</>
-  );
+  return ( 
+  <>
+   petroman
+   <ResponsiveDrawer/>
+ </> );
 }
