9bb0c10cd27d2d6bdb16ff65d878301bddb77c11
AbhijithGopalakrishnan <abhijithpg13579@gmail.com>
2024-05-31 21:50:06 +0530

    latest

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,42 +1,45 @@
-"use client"
+"use client";
 
-import  React,{useState} from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Link from "next/link"
-import ResponsiveAppBar from './appbar/livedate';
-import { Avatar, Menu, MenuItem } from '@mui/material';
-import Image from 'next/image';
-import moment from 'moment';
-import { AccountCircle } from '@mui/icons-material';
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MailIcon from "@mui/icons-material/Mail";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Link from "next/link";
+import { Avatar, Menu, MenuItem } from "@mui/material";
+import Image from "next/image";
+import moment from "moment";
+import { AccountCircle } from "@mui/icons-material";
+import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import GroupsIcon from '@mui/icons-material/Groups';
+import ArticleIcon from '@mui/icons-material/Article';
+
 const drawerWidth = 240;
-
 
 export default function ResponsiveDrawer() {
   // const { window } = props;
   const [mobileOpen, setMobileOpen] = useState(false);
   const [isClosing, setIsClosing] = useState(false);
 
-  const datePart = moment().format('DD/MM/YYYY');
-const dayPart = moment().format('dddd');
-const [anchorEl, setAnchorEl] = React.useState(null);
-const handleClose = () => {
-  setAnchorEl(null);
-}
+  const datePart = moment().format("DD/MM/YYYY");
+  const dayPart = moment().format("dddd");
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const handleDrawerClose = () => {
     setIsClosing(true);
@@ -57,31 +60,26 @@ const handleClose = () => {
     setAnchorEl(event.currentTarget);
   };
 
-  const routes = {
-    Dashboard: '/dashboard',
-    Accounts: '/accounts',
-    Employees: '/employe',
-    Reports: '/reports',
-  };
+  const routes = [
+    { name: "Dashboard", link: "/dashboard", icon: <SpaceDashboardIcon /> },
+    { name: "Accounts", link: "/accounts", icon: <AccountBalanceWalletIcon /> },
+    { name: "Employee", link: "/employe", icon: <GroupsIcon /> },
+    { name: "Reports", link: "/reports", icon: <ArticleIcon /> },
+  ];
 
   const drawer = (
-    <div >
+    <div>
       <Toolbar>
-      <Image src="/Petrobytes.png" width={120}height={40} style={{backgroundColor:"black"}}/>
-
-
+        <Image src="/Petro.png" width={140} height={60} />
       </Toolbar>
-      
-      {/* <Divider /> */}
-      
+      {/* <Divider /> */} 
       <List>
-        {['Dashboard', 'Accounts', 'Employees','Reports'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton component={Link} to={routes[text]}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
+         
+        {routes.map((item, index) => (
+          <ListItem key={item.link} disablePadding>
+            <ListItemButton component={Link} to={item.link}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.name} />
             </ListItemButton>
           </ListItem>
         ))}
@@ -106,63 +104,57 @@ const handleClose = () => {
   // const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar
-      elevation={1}
+        elevation={1}
         position="fixed"
-        
         sx={{
           width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },       
-          backgroundColor: 'white',
-
+          ml: { sm: `${drawerWidth}px` },
+          backgroundColor: "white",
         }}
       >
-        <Toolbar sx={{display:"flex",justifyContent:"space-between"}}>
-          
-          <Box sx={{color:"black",display:"flex",flexDirection:"column"}}>
-          <Typography sx={{fontStyle:"italic"}}>
-            {datePart}
-            
-          </Typography>
-          <Typography sx={{fontStyle:"italic"}}>{dayPart}</Typography>
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box
+            sx={{ color: "black", display: "flex", flexDirection: "column" }}
+          >
+            <Typography sx={{ fontStyle: "inherit" }}>{datePart}</Typography>
+            <Typography sx={{ fontStyle: "inherit" }}>{dayPart}</Typography>
           </Box>
-          <Box sx={{alignItems:"end"}}>
-          <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="success"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>Log out</MenuItem>
-              </Menu>
-              </Box>
-              
-              
+          <Box sx={{ alignItems: "end" }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="success"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>Log out</MenuItem>
+            </Menu>
+          </Box>
         </Toolbar>
       </AppBar>
-    
+
       <Box
         component="nav"
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
@@ -170,7 +162,7 @@ const handleClose = () => {
       >
         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Drawer
-        //  container={container}
+          //  container={container}
           variant="temporary"
           open={mobileOpen}
           onTransitionEnd={handleDrawerTransitionEnd}
@@ -179,8 +171,11 @@ const handleClose = () => {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
@@ -188,8 +183,11 @@ const handleClose = () => {
         <Drawer
           variant="permanent"
           sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
           open
         >
@@ -198,10 +196,13 @@ const handleClose = () => {
       </Box>
       <Box
         component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
       >
         <Toolbar />
-     
       </Box>
     </Box>
   );
